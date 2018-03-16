cb1f99f4fbaf6accb0ea13ad957440c4e28e7da5
Gal Tashma <galtashma@gmail.com>
2018-03-16 14:22:05 +0200

    Updated Schema listView list item design

--- app/src/main/java/com/galtashma/parsedashboard/SchemaListAdapter.java
+++ app/src/main/java/com/galtashma/parsedashboard/SchemaListAdapter.java
@@ -1,15 +1,18 @@
 package com.galtashma.parsedashboard;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.galtashma.lazyparse.LazyList;
 import com.galtashma.lazyparse.LazyParseObjectHolder;
 import com.galtashma.lazyparse.ScrollInfiniteAdapter;
+import com.lucasurbas.listitemview.ListItemView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by gal on 3/9/18.
@@ -18,22 +21,29 @@ import com.galtashma.lazyparse.ScrollInfiniteAdapter;
 public class SchemaListAdapter extends ScrollInfiniteAdapter<LazyParseSchema> {
 
     public SchemaListAdapter(Context context, LazyList<LazyParseSchema> lazyValues) {
-        super(context, lazyValues, android.R.layout.simple_list_item_1, 15);
+        super(context, lazyValues, R.layout.list_item, 15);
     }
 
     @Override
     public View renderReadyLazyObject(final LazyParseSchema lazyParseSchema, View view, @NonNull ViewGroup viewGroup) {
-        final TextView tv = view.findViewById(android.R.id.text1);
-        tv.setTextColor(Color.BLACK);
-        tv.setText(lazyParseSchema.getClassName());
+        ListItemView item = (ListItemView) view;
+        item.setTitle(lazyParseSchema.getClassName());
+        item.setSubtitle(getSchemaString(lazyParseSchema));
+
         return view;
     }
 
     @Override
     public View renderLoadingLazyObject(LazyParseObjectHolder<LazyParseSchema> lazyParseObjectHolder, View view, @NonNull ViewGroup viewGroup) {
-        final TextView tv = view.findViewById(android.R.id.text1);
-        tv.setTextColor(Color.BLACK);
-        tv.setText("Loading...");
+        ListItemView item = (ListItemView) view;
+        item.setTitle("Loading...");
         return view;
+    }
+
+
+    private String getSchemaString(LazyParseSchema schema){
+        List<String> list = new ArrayList<String>(schema.getFields().keySet());
+        Collections.sort(list);
+        return list.toString().replaceAll("\\[|\\]","").replaceAll(","," ");
     }
 }
