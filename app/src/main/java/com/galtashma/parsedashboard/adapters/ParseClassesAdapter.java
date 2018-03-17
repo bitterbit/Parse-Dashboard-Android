e8b3b19c8ad16b17b9ead7878cee091ccdff60ee
Gal Tashma <galtashma@gmail.com>
2018-03-17 14:33:51 +0200

    Converted Parse App Activity to use the new parse schema api

--- app/src/main/java/com/galtashma/parsedashboard/adapters/ParseClassesAdapter.java
+++ app/src/main/java/com/galtashma/parsedashboard/adapters/ParseClassesAdapter.java
@@ -2,50 +2,68 @@ package com.galtashma.parsedashboard.adapters;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
-import com.galtashma.lazyparse.LazyList;
-import com.galtashma.lazyparse.LazyParseObjectHolder;
-import com.galtashma.lazyparse.ScrollInfiniteAdapter;
-import com.galtashma.parsedashboard.LazyParseSchema;
 import com.galtashma.parsedashboard.R;
 import com.lucasurbas.listitemview.ListItemView;
+import com.parse.ParseSchema;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * Created by gal on 3/9/18.
- */
 
-public class ParseClassesAdapter extends ScrollInfiniteAdapter<LazyParseSchema> {
+public class ParseClassesAdapter extends ArrayAdapter<ParseSchema> {
 
-    public ParseClassesAdapter(Context context, LazyList<LazyParseSchema> lazyValues) {
-        super(context, lazyValues, R.layout.list_item, 15);
+    public interface OnClickListener{
+        void onSchemaClicked(ParseSchema schema);
     }
 
-    @Override
-    public View renderReadyLazyObject(final LazyParseSchema lazyParseSchema, View view, @NonNull ViewGroup viewGroup) {
-        ListItemView item = (ListItemView) view;
-        item.setTitle(lazyParseSchema.getClassName());
-        item.setSubtitle(getSchemaString(lazyParseSchema));
+    private OnClickListener listener;
 
-        return view;
+    public ParseClassesAdapter(@NonNull Context context) {
+        super(context, R.layout.list_item);
     }
 
-    @Override
-    public View renderLoadingLazyObject(LazyParseObjectHolder<LazyParseSchema> lazyParseObjectHolder, View view, @NonNull ViewGroup viewGroup) {
-        ListItemView item = (ListItemView) view;
-        item.setTitle("Loading...");
-        return view;
+    public ParseClassesAdapter(@NonNull Context context, @NonNull List<ParseSchema> objects) {
+        super(context, R.layout.list_item, objects);
     }
 
+    @NonNull
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        final ParseSchema schema = getItem(position);
+        if(convertView == null) {
+            convertView = LayoutInflater.from(this.getContext()).inflate(R.layout.list_item, parent, false);
+        }
 
-    private String getSchemaString(LazyParseSchema schema){
+        ListItemView item = (ListItemView) convertView;
+        item.setTitle(schema.getName());
+        item.setSubtitle(getSchemaString(schema));
+
+        item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(listener != null){
+                    listener.onSchemaClicked(schema);
+                }
+            }
+        });
+
+        return convertView;
+
+    }
+
+    private String getSchemaString(ParseSchema schema){
         List<String> list = new ArrayList<String>(schema.getFields().keySet());
         Collections.sort(list);
         return list.toString().replaceAll("\\[|\\]","").replaceAll(","," ");
+    }
+
+
+    public void setListener(OnClickListener listener) {
+        this.listener = listener;
     }
 }
--- app/src/main/java/com/galtashma/parsedashboard/screens/SingleAppParseActivity.java
+++ app/src/main/java/com/galtashma/parsedashboard/screens/SingleAppParseActivity.java
@@ -4,28 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.afollestad.ason.Ason;
-import com.galtashma.lazyparse.LazyList;
-import com.galtashma.lazyparse.ScrollInfiniteAdapter;
-import com.galtashma.lazyparse.ScrollInfiniteListener;
 import com.galtashma.parsedashboard.Const;
-import com.galtashma.parsedashboard.LazyParseSchema;
 import com.galtashma.parsedashboard.ParseServerConfig;
 import com.galtashma.parsedashboard.R;
 import com.galtashma.parsedashboard.adapters.ParseClassesAdapter;
 import com.parse.Parse;
-import com.parse.ParseObject;
-import com.parse.ParseSchemaQuery;
+import com.parse.ParseSchema;
 
+import java.util.List;
+
+import bolts.Continuation;
+import bolts.Task;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 
 
 public class SingleAppParseActivity extends AppCompatActivity {
+
+    private ParseClassesAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,21 +48,33 @@ public class SingleAppParseActivity extends AppCompatActivity {
         initParse(config.appId, config.serverUrl, config.masterKey);
         setTitle(config.appName);
 
-        // initParse(getString(R.string.parse_app_id), getString(R.string.parse_server_url), getString(R.string.parse_master_key));
-
-        ParseSchemaQuery<LazyParseSchema> query = LazyParseSchema.getQuery();
-
-        LazyList<LazyParseSchema> list = new LazyList<LazyParseSchema>(query);
-        ParseClassesAdapter adapter  = new ParseClassesAdapter(this, list);
-
+        adapter = new ParseClassesAdapter(this);
         ListView listView = findViewById(R.id.list_view_view);
         listView.setAdapter(adapter);
-        listView.setOnScrollListener(new ScrollInfiniteListener(adapter));
 
-        adapter.setOnClickListener(new ScrollInfiniteAdapter.OnClickListener<LazyParseSchema>(){
+        ParseSchema.getParseSchemasAsync().onSuccess(new Continuation<List<ParseSchema>, Void>() {
             @Override
-            public void onClick(LazyParseSchema parseObject) {
-                showTable(parseObject.getClassName());
+            public Void then(Task<List<ParseSchema>> task) throws Exception {
+                Log.i(Const.TAG, "found schemas " + task.getResult());
+                updateListOnUIThread(task.getResult());
+                return null;
+            }
+        });
+
+        adapter.setListener(new ParseClassesAdapter.OnClickListener() {
+            @Override
+            public void onSchemaClicked(ParseSchema schema) {
+                showTable(schema.getName());
+            }
+        });
+    }
+
+    private void updateListOnUIThread(final List<ParseSchema> schemas){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                adapter.addAll(schemas);
+                adapter.notifyDataSetChanged();
             }
         });
     }
@@ -76,8 +90,6 @@ public class SingleAppParseActivity extends AppCompatActivity {
         HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
         httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         builder.networkInterceptors().add(httpLoggingInterceptor);
-
-        ParseObject.registerSubclass(LazyParseSchema.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(appId)
