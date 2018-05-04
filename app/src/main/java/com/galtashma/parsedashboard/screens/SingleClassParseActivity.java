f2c88a036b5e6f8074629c4fa5f1912961d4610f
Gal Tashma <galtashma@gmail.com>
2018-05-04 18:00:45 +0300

    Added refresh to class activity

--- app/src/main/java/com/galtashma/parsedashboard/screens/SingleClassParseActivity.java
+++ app/src/main/java/com/galtashma/parsedashboard/screens/SingleClassParseActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.crashlytics.android.answers.Answers;
@@ -20,6 +22,9 @@ import com.parse.ParseQuery;
 import com.vlonjatg.progressactivity.ProgressRelativeLayout;
 
 public class SingleClassParseActivity extends AppCompatActivity implements ScrollInfiniteAdapter.OnClickListener<ParseObject> {
+
+    private String className;
+    ProgressRelativeLayout statefulLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,30 +44,34 @@ public class SingleClassParseActivity extends AppCompatActivity implements Scrol
             return;
         }
 
-        String className = extra.getString(Const.BUNDLE_KEY_CLASS_NAME);
+        className = extra.getString(Const.BUNDLE_KEY_CLASS_NAME);
         setTitle(className);
 
-        ProgressRelativeLayout statefulLayout = findViewById(R.id.stateful_layout);
-        statefulLayout.showContent();
-
-        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(className);
-        LazyList<ParseObject> list = new LazyList<ParseObject>(query);
-        ParseObjectsAdapter adapter  = new ParseObjectsAdapter(this, list);
-
+        statefulLayout = findViewById(R.id.stateful_layout);
         ListView listView = findViewById(R.id.list_view);
-        listView.setAdapter(adapter);
-        listView.setOnScrollListener(new ScrollInfiniteListener(adapter));
-
-        adapter.setOnClickListener(this);
-
-        if (list.getLimit() == 0){
-            statefulLayout.showEmpty(R.drawable.ic_parse_24dp, getString(R.string.empty_state_objects_screen_short), getString(R.string.empty_state_objects_screen_long));
-        }
+        initList(listView);
 
         Answers.getInstance().logContentView(new ContentViewEvent()
                 .putContentId(Hash.sha1(className))
                 .putContentName("Class Activity")
                 .putContentType("Screen"));
+    }
+
+    private void initList(ListView listView){
+        statefulLayout.showLoading();
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(className);
+        LazyList<ParseObject> list = new LazyList<ParseObject>(query);
+        ParseObjectsAdapter adapter  = new ParseObjectsAdapter(this, list);
+        listView.setAdapter(adapter);
+        listView.setOnScrollListener(new ScrollInfiniteListener(adapter));
+        adapter.setOnClickListener(this);
+
+        if (list.getLimit() == 0){
+            statefulLayout.showEmpty(R.drawable.ic_parse_24dp, getString(R.string.empty_state_objects_screen_short), getString(R.string.empty_state_objects_screen_long));
+            return;
+        }
+
+        statefulLayout.showContent();
     }
 
     @Override
@@ -71,5 +80,14 @@ public class SingleClassParseActivity extends AppCompatActivity implements Scrol
         i.putExtra(Const.BUNDLE_KEY_CLASS_NAME, parseObject.getClassName());
         i.putExtra(Const.BUNDLE_KEY_OBJECT_ID, parseObject.getObjectId());
         this.startActivityForResult(i, 1);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_class_view, menu);
+        return true;
+    }
+
+    public void onRefresh(MenuItem item) {
+        initList((ListView) findViewById(R.id.list_view));
     }
 }
--- app/src/main/java/com/galtashma/parsedashboard/screens/SingleObjectParseActivity.java
+++ app/src/main/java/com/galtashma/parsedashboard/screens/SingleObjectParseActivity.java
@@ -140,11 +140,8 @@ public class SingleObjectParseActivity extends AppCompatActivity implements GetC
         return true;
     }
 
-
-    // Menu icons are inflated just as they were with actionbar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_object_view, menu);
         return true;
     }
